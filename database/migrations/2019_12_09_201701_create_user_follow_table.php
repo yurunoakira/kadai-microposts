fdf0cf24bc4c415f02299897b6e36ef0de85c484
akira.yuruno <zinnai1117@yahoo.co.jp>
2019-12-11 04:46:27 +0000

    retry

--- database/migrations/2019_12_09_201701_create_user_follow_table.php
+++ database/migrations/2019_12_09_201701_create_user_follow_table.php
@@ -11,7 +11,7 @@ class CreateUserFollowTable extends Migration
      *
      * @return void
      */
-     public function up()
+    public function up()
     {
         Schema::create('user_follow', function (Blueprint $table) {
             $table->increments('id');
